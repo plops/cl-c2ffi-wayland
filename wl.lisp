528f1b6041a99d08a85a2f0da7d0852e1e4c06ba
Martin Kielhorn <martin.kielhorn@ezono.com>
2016-05-18 23:34:51 +0200

    i don't want to parse some sources. in particular i don't need math.h [martin@localhost include]$ grep \\#include wayland-*|grep '<'|cut -d : -f 2|sor t|uniq #include <inttypes.h> #include <math.h> #include <stdarg.h> #include <stddef.h> #include <stdint.h> #include <sys/types.h> #include <wayland-client.h>

--- wl.lisp
+++ wl.lisp
@@ -1,19 +1,61 @@
 
-(eval-when (:compile-toplevel) (ql:quickload :cl-autowrap))
+(eval-when (:compile-toplevel)
+  (ql:quickload :cl-autowrap)
+  (ql:quickload :rutilsx))
 (defpackage :wl
-  (:use  :cl :autowrap))
+  (:use  :cl #:autowrap #:rutil #:rutilsx
+	 #:rutilsx.threading))
 (in-package :wl)
+(named-readtables:in-readtable rutils-readtable)
+
+(eval-always
+ (defpar *spec-path* (merge-pathnames
+		      "stage/cl-c2ffi-wayland/"
+		      (user-homedir-pathname))))
+
 (autowrap:c-include "/usr/include/wayland-client.h"
-		    :spec-path "/dev/shm/"
+		    :spec-path *spec-path*
 		    :exclude-arch ("arm-pc-linux-gnu"
-"i386-unknown-freebsd"
-"i686-apple-darwin9"
-"i686-pc-linux-gnu"
-"i686-pc-windows-msvc"
-"x86_64-apple-darwin9"
-;"x86_64-pc-linux-gnu"
-"x86_64-pc-windows-msvc"
-"x86_64-unknown-freebsd"
-)
-		    :exclude-definitions ("asin" "sin" "acos" "cos" "atan" "tan" "exp" "log"
-						 "sqrt" "floor" "round"))
+				   "i386-unknown-freebsd"
+				   "i686-apple-darwin9"
+				   "i686-pc-linux-gnu"
+				   "i686-pc-windows-msvc"
+				   "x86_64-apple-darwin9"
+					;"x86_64-pc-linux-gnu"
+				   "x86_64-pc-windows-msvc"
+				   "x86_64-unknown-freebsd")
+		    :exclude-sources ("/usr/include/inttypes.h"
+				      "/usr/include/math.h"
+				      "/usr/include/stdarg.h"
+				      "/usr/include/stddef.h"
+				      "/usr/include/stdint.h"
+				      "/usr/include/sys/types.h")
+		    ;:exclude-definitions
+		    #+nil ("asin" "sin"  "acos" "cos"
+			    "atan" "tan" "exp" "log"
+			    "sqrt" "floor" "round"))
+
+(autowrap:c-include "/usr/include/wayland-client-protocol.h"
+		    :spec-path *spec-path*
+		    :exclude-arch ("arm-pc-linux-gnu"
+				   "i386-unknown-freebsd"
+				   "i686-apple-darwin9"
+				   "i686-pc-linux-gnu"
+				   "i686-pc-windows-msvc"
+				   "x86_64-apple-darwin9"
+					;"x86_64-pc-linux-gnu"
+				   "x86_64-pc-windows-msvc"
+				   "x86_64-unknown-freebsd")
+		    :exclude-definitions
+		    ("asin" "sin"  "acos" "cos"
+			    "atan" "tan" "exp" "log"
+			    "sqrt" "floor" "round"))
+
+#+ni
+(cffi:use-foreign-library "libwayland-client.so")
+
+#+nil
+(defpar *display* (wl-display-connect (cffi:null-pointer)))
+
+#+nil
+(defpar *registry* (wl-display-))
